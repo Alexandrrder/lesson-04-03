a5fa77f98da91773785a7012d01f5a9f6a004016
alexandrrder <alexandrrder@gmail.com>
2025-01-09 10:41:17 +0100

    secnd commit

---
+++ lesson-04/03.js
@@ -0,0 +1,29 @@
+/*
+Напишите функцию `findCommonElements`, которая принимает два массива и возвращает массив, содержащий общие элементы из обоих массивов.
+
+Входные данные:
+
+- `array1`: Первый массив, содержащий любые типы данных.
+- `array2`: Второй массив, содержащий любые типы данных.
+
+Выходные данные:
+
+- Массив, содержащий элементы, которые присутствуют в обоих исходных массивах.
+- Если общих элементов нет, должен вернуться пустой массив
+
+Пример использования:
+findCommonElements([1, 2, 3], [2, 3, 4]) // [2, 3]
+
+Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
+*/
+
+function findCommonElements(array1, array2) {
+   const commonElements = [];
+   for (let element of array1) {
+    if(array2.includes(element) && !commonElements.includes(element)){
+        commonElements.push(element);
+    }
+   }
+   return commonElements;
+}
+console.log(findCommonElements([1, 2, 3], [2, 3, 4]));
